bd851ce2ab9a1f56b0b08c53c7190aa44ece104c
Gianpaolo Papa <papa.gianpaolo@gmail.com>
2019-01-04 04:49:35 +1100

    first commit

---
+++ src/utils/siteConfig.js
@@ -0,0 +1,21 @@
+module.exports = {
+  siteTitle: 'Luca Papa',
+  siteTitleAlt: 'Luca Papa', // This allows an alternative site title for SEO schema.
+  publisher: 'Luca Papa', // Organization name used for SEO schema
+  siteDescription:
+    'Luca Papa Portfolio and Services',
+  siteUrl: 'http://lucapapa-20190104043001-hostingbucket.s3-website-us-west-2.amazonaws.com', // Site domain. Do not include a trailing slash! If you wish to use a path prefix you can read more about that here: https://www.gatsbyjs.org/docs/path-prefix/
+  postsPerHomePage: 7, // Number of posts shown on the 1st page of of the index.js template (home page)
+  postsPerPage: 6, // Number of posts shown on paginated pages
+  author: 'Luca Papa', // Author for RSS author segment and SEO schema
+  authorUrl: 'http://lucapapa-20190104043001-hostingbucket.s3-website-us-west-2.amazonaws.com/about/', // URL used for author and publisher schema, can be a social profile or other personal site
+  userTwitter: '@twitter', // Change for Twitter Cards
+  shortTitle: 'Luca Papa App', // Used for App manifest e.g. Mobile Home Screen
+  shareImage: '/logos/share.jpg', // Open Graph Default Share Image. 1200x1200 is recommended
+  shareImageWidth: 900, // Change to the width of your default share image
+  shareImageHeight: 600, // Change to the height of your default share image
+  siteLogo: '/logos/logo-512.png', // Logo used for SEO, RSS, and App manifest
+  backgroundColor: '#e9e9e9', // Used for Offline Manifest
+  themeColor: '#121212', // Used for Offline Manifest
+  copyright: 'Copyright © 2019 Luca Papa', // Copyright string for the RSS feed
+}
